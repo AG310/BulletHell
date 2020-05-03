c3c593d6b5bbd2b012d1955a12462dd6150458d3
AG310 <andreweg@usc.edu>
2020-05-03 13:42:05 -0700

    Added AnimationTimer Game Loop, Added Keyboard Input Reading in Main

--- BulletHell/src/Main.java
+++ BulletHell/src/Main.java
@@ -1,6 +1,9 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -11,17 +14,20 @@ public class Main extends Application{
 	private AnchorPane mainPane;
 	private Pane gameField;
 	private Character player;
-	
+	private Scene scene;
+	private AnimationTimer gameLoop;
 	@Override
 	public void start(Stage stage){
 		initialize(stage);
-		
+		createGameInputs();
+		createGameLoop();
+		gameLoop.start();
 		
 	}
 	
 	public void initialize(Stage stage) {
 		mainPane = new AnchorPane();
-		Scene scene = new Scene(mainPane,500, 600);
+		scene = new Scene(mainPane,500, 600);
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.setTitle("Bullet Hell!");
@@ -43,6 +49,53 @@ public class Main extends Application{
 		player = new Character(gameField);
 		gameField.getChildren().add(player);
 		player.refreshLocation();
+	}
+	
+	public void createGameInputs() {
+		scene.addEventHandler(KeyEvent.KEY_PRESSED, 
+				ev->{
+					KeyCode code = ev.getCode();
+					if(code==KeyCode.UP) {
+						player.moveUp();
+					}
+					if(code==KeyCode.DOWN) {
+						player.moveDown();
+					}
+					if(code==KeyCode.LEFT) {
+						player.moveLeft();
+					}
+					if(code==KeyCode.RIGHT) {
+						player.moveRight();
+					}
+				}
+		);
+		
+		scene.addEventHandler(KeyEvent.KEY_RELEASED, 
+				ev->{
+					KeyCode code = ev.getCode();
+					if(code==KeyCode.UP) {
+						player.stopYMovement();
+					}
+					if(code==KeyCode.DOWN) {
+						player.stopYMovement();
+					}
+					if(code==KeyCode.LEFT) {
+						player.stopXMovement();
+					}
+					if(code==KeyCode.RIGHT) {
+						player.stopXMovement();
+					}
+				});
+	}
+	
+	public void createGameLoop() {
+		gameLoop = new AnimationTimer() {
+			
+			@Override
+			public void handle(long now) {
+				player.update();
+			}
+		};
 	}
 	
 	public static void main(String[] args) {
