87271917e05c180563ff58d1eb852072f668b0c3
AG310 <andreweg@usc.edu>
2020-05-04 14:30:15 -0700

    Add Titles and Transitions to Main, Refactor Game Loop for efficiency

--- BulletHell/src/Main.java
+++ BulletHell/src/Main.java
@@ -1,16 +1,26 @@
+import java.net.URISyntaxException;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Duration;
 
 public class Main extends Application{
 	/*
@@ -23,27 +33,32 @@ public class Main extends Application{
 	private Scene scene;
 	private AnimationTimer gameLoop;
 	private BulletSpawner bulletSpawner;
-	//private 
+	private ImageView hellTitle; 
+	private ImageView girlTitle;
+	private ImageView startTitle;
+	private AudioPlayer audioPlayer;
 	
 	@Override
 	public void start(Stage stage){
 		initialize(stage);
 		createGameInputs();
 		createGameLoop();
-		gameLoop.start();
-		
+		createTitles();
+		startingTitleTransition();	
+		audioPlayer.playWind();
 		//Create Thread to monitor if player is still alive to continue
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				//TODO put label score keeping in loop
-				while(!player.isDead()) {System.out.println();}
-				//Stop game loop when player dies
-				gameLoop.stop();
-			}
-		}).start();
+//		new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				//TODO put label score keeping in loop
+//				while(!player.isDead()) {System.out.println();}
+//				//Stop game loop when player dies
+//				gameLoop.stop();
+//			}
+//		}).start();
+		removeAllBullets();
 	}
-	
+	 
 	/*
 	 * Initialize variables and wire up panes
 	 */
@@ -77,12 +92,125 @@ public class Main extends Application{
 		
 		//Create our list of bullets the player will have to dodge
 		bulletList = new LinkedList<Bullet>();
-		bulletList.add(new Bullet(gameField,50, 50, 1, 1));
 		gameField.getChildren().addAll(bulletList);
 		
 		//Create a BulletSpawer to spawn bullets
 		bulletSpawner = new BulletSpawner(gameField);
+		
+		//Create Audio Player for game sounds
+		audioPlayer = new AudioPlayer();
 	}
+	
+	
+	public void createTitles()
+	{
+		try {
+			hellTitle = new ImageView(new Image(getClass().getResource("hellTitle.png").toURI().toString()));
+			girlTitle = new ImageView(new Image(getClass().getResource("girlTitle.png").toURI().toString()));
+			startTitle = new ImageView(new Image(getClass().getResource("startGlow.png").toURI().toString())); 
+			mainPane.getChildren().addAll(hellTitle, girlTitle, startTitle);
+			AnchorPane.setTopAnchor(hellTitle, 90.0);
+			AnchorPane.setLeftAnchor(hellTitle, -950.0);
+			AnchorPane.setTopAnchor(girlTitle, 180.0);
+			AnchorPane.setLeftAnchor(girlTitle, 1245.0);
+			AnchorPane.setTopAnchor(startTitle, 350.0);
+			AnchorPane.setLeftAnchor(startTitle, 110.0);
+			
+			startTitle.setOnMouseEntered(ev ->
+			{
+				startTitle.setEffect(new Glow(0.9));
+				audioPlayer.playCursorHover();
+			});
+			
+			startTitle.setOnMouseExited(ev->{
+				startTitle.setEffect(null);
+			});
+			
+			startTitle.addEventHandler(MouseEvent.MOUSE_CLICKED, 
+					ev->{
+						startTitle.setVisible(false);
+						endingTitleTransition();
+						new Thread(new Runnable() {
+							@Override
+							public void run() {
+								try {
+									Thread.sleep(500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+								audioPlayer.stopWind();
+								audioPlayer.playMusic();
+								gameLoop.start();
+							}
+						}).start();
+					});
+			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Create Starting Transition for the Title When Application is opened
+	 */
+	public void startingTitleTransition() {
+		//Create Hell title transitions
+		TranslateTransition hellTransition = new TranslateTransition();
+		hellTransition.setNode(hellTitle);
+		hellTransition.setByX(1000);
+		hellTransition.setDuration(Duration.seconds(0.5));
+		FadeTransition hellFadeTransition = new FadeTransition(Duration.millis(100));
+		hellFadeTransition.setNode(hellTitle);
+		hellFadeTransition.setDelay(Duration.seconds(0.1));
+		hellFadeTransition.setFromValue(1);
+		hellFadeTransition.setToValue(0);
+		hellFadeTransition.setAutoReverse(true);
+		hellFadeTransition.setCycleCount(10);
+		//Create Girl title 
+		TranslateTransition girlTransition = new TranslateTransition();
+		girlTransition.setNode(girlTitle);
+		girlTransition.setByX(-1000);
+		girlTransition.setDuration(Duration.seconds(0.5));
+		FadeTransition girlFadeTransition = new FadeTransition(Duration.millis(100));
+		girlFadeTransition.setNode(girlTitle);
+		girlFadeTransition.setDelay(Duration.seconds(0.1));
+		girlFadeTransition.setFromValue(1);
+		girlFadeTransition.setToValue(0);
+		girlFadeTransition.setAutoReverse(true);
+		girlFadeTransition.setCycleCount(10);
+		
+		//Make Transition for Start Game Title
+		FadeTransition startTransition = new FadeTransition(Duration.millis(1000), startTitle);
+		startTransition.setDelay(Duration.seconds(0.2));
+		startTransition.setFromValue(1);
+		startTransition.setToValue(0);
+		startTransition.setAutoReverse(true);
+		startTransition.setCycleCount(10);
+		
+		//Make Sequential Transitions and play them
+		SequentialTransition hellSequen = new SequentialTransition();
+		hellSequen.getChildren().addAll(hellTransition, hellFadeTransition);
+		SequentialTransition girlSequen = new SequentialTransition();
+		girlSequen.getChildren().addAll(girlTransition, girlFadeTransition);
+		
+		hellSequen.play();
+		girlSequen.play();
+		startTransition.play();
+	}
+	
+	public void endingTitleTransition() {
+		TranslateTransition hellTransition = new TranslateTransition();
+		hellTransition.setNode(hellTitle);
+		hellTransition.setByX(1000);
+		hellTransition.setDuration(Duration.seconds(0.5));
+		TranslateTransition girlTransition = new TranslateTransition();
+		girlTransition.setNode(girlTitle);
+		girlTransition.setByX(-1000);
+		girlTransition.setDuration(Duration.seconds(0.5));
+		hellTransition.play();
+		girlTransition.play();
+	}
+	
 	
 	/*
 	 * Create our inputs to move the Player 
@@ -179,8 +307,9 @@ public class Main extends Application{
 				//Spawn bullets if timer allows it
 				else if (now-prevTime > spawnTimer) {
 					LinkedList<Bullet> newBullets = bulletSpawner.spawnRandomBullets();
-					bulletList.addAll(bulletSpawner.spawnRandomBullets());
+					bulletList.addAll(newBullets);
 					gameField.getChildren().addAll(newBullets);
+					newBullets.clear();
 					prevTime = now;
 				}
 				
